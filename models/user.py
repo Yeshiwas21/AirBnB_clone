c0e6c5f4fecd7a366fb71a877ae5763416959a74
Yeshiwas Dagnaw <yeshiwasdagnaw@gmail.com>
2023-10-16 10:15:29 +0300

    Updated

--- models/user.py
+++ models/user.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 """Defines the User class."""
+
 from models.base_model import BaseModel
 
-
 class User(BaseModel):
-    """Represent a User.
-
-    Attributes:
-        email (str): The email of the user.
-        password (str): The password of the user.
-        first_name (str): The first name of the user.
-        last_name (str): The last name of the user.
     """
+    The User class inherits from BaseModel and represents a User object.
 
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    Public instance attributes:
+    - username: string - the user's username.
+    - email: string - the user's email.
+
+    Additional user-specific attributes can be added as needed.
+    """
+    
+    def __init__(self, username, email):
+        super().__init__()
+        self.username = username
+        self.email = email
+        # Additional user-specific attributes can be added here
