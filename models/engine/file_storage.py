5476663450d5306172418407a057230844419b28
Yeshiwas Dagnaw <yeshiwasdagnaw@gmail.com>
2023-12-30 13:30:25 +0300

    File Storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,42 +1,31 @@
-#!/usr/bin/python3
-"""Module for FileStorage class."""
+# models/engine/file_storage.py
 import json
-from models.base_model import BaseModel
-from datetime import datetime
-
+from models.base_model import BaseModel  # Import BaseModel locally within methods
 
 class FileStorage:
-    """Define the FileStorage class."""
-
-    __file_path = "file.json"
-    __objects = {}
-
     def all(self):
-        """Return the dictionary __objects."""
         return self.__objects
 
     def new(self, obj):
-        """Set in __objects the obj with key <obj class name>.id."""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
 
     def save(self):
-        """Serialize __objects to the JSON file (path: __file_path)."""
-        with open(self.__file_path, mode='w', encoding='utf-8') as file:
-            obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
-            json.dump(obj_dict, file)
+        data = {}
+        for key, value in self.__objects.items():
+            data[key] = value.to_dict()
+        with open(self.__file_path, 'w') as file:
+            json.dump(data, file)
 
     def reload(self):
-        """Deserialize the JSON file to __objects."""
         try:
-            with open(self.__file_path, mode='r', encoding='utf-8') as file:
-                obj_dict = json.load(file)
-                for key, value in obj_dict.items():
+            with open(self.__file_path, 'r') as file:
+                data = json.load(file)
+                for key, value in data.items():
                     class_name, obj_id = key.split('.')
-                    value['created_at'] = datetime.strptime(value['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
-                    value['updated_at'] = datetime.strptime(value['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
-                    obj_instance = BaseModel(**value) if class_name == 'BaseModel' else None
-                    if obj_instance:
-                        self.__objects[key] = obj_instance
+                    cls = eval(class_name)  # Use eval to dynamically get the class
+                    obj = cls(**value)
+                    self.__objects[key] = obj
         except FileNotFoundError:
             pass
+
